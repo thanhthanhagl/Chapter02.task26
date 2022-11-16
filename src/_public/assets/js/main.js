ae643c5d3dae43ea3041a4891d591417890c573c
thanhthanh54 <thanhuitk13@gmail.com>
2022-11-16 23:32:55 +0700

    update layout and slide

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -2,13 +2,16 @@
 
 //console.log("main.js");
 $(document).ready(function () {
-    $('.c-slider').slick({
+    $('.c-design__slider').slick({
         infinite: true,//loop
         dots: false,
-        arrows: false,
+        arrows: true,
         autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        prevArrow:'<button type="button" class="c-design__prev"></button>',
+        nextArrow:'<button type="button" class="c-design__next"></button>',
+        cssEase: 'linear'
     });
 });
