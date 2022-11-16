3d8f56c20d1b932706e0c71f878fb65135d4ff33
thanhthanhagl <diepthanhthanh.agl@gmail.com>
2022-11-16 17:56:09 +0700

    task 26

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,14 @@
-console.log("main.js");
+//const { Value } = require("sass");
+
+//console.log("main.js");
+$(document).ready(function () {
+    $('.c-slider').slick({
+        infinite: true,//loop
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
